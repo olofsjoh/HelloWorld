f566a1fce95556a78c96d7d50aac5e25ca7690d2
Johannes Olofsson <johannesolofsson@hotmail.com>
2020-12-26 14:40:32 +0100

    Fixed warnings

--- src/com/example/helloworld/Main.java
+++ src/com/example/helloworld/Main.java
@@ -1,18 +1,18 @@
 package com.example.helloworld;
 import org.jool.ecommerce.*;
 
-import java.util.StringTokenizer;
+//import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
         StoreFront storeFront = new StoreFront();
         storeFront.addItem("CO1", "MUG", "9.99", "150");
         storeFront.addItem("CO2", "LG MUG", "12.99", "82");
-        storeFront.addItem("CO3", "MOUSEPAD", "10.49", "800");
+        storeFront.addItem("CO3", "MOUSE PAD", "10.49", "800");
         storeFront.addItem("DO1", "T SHIRT", "16.99", "90");
 
         for(int i=0; i< storeFront.getSize(); i++){
-            Item item = (Item)storeFront.getItem(i);
+            Item item = storeFront.getItem(i);
             System.out.println("\nItem ID: " + item.getId() +
                                "\nName: " + item.getName() +
                                "\nRetail Price: $" + item.getRetail() +
